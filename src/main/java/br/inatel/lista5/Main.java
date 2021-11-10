f81e6c2cba55aa12699d0be3b3653b8ac7cd16af
Pedrolly <86805506+Pedrolly@users.noreply.github.com>
2021-11-10 16:15:52 -0300

    main.commit

--- src/main/java/br/inatel/lista5/Main.java
+++ src/main/java/br/inatel/lista5/Main.java
@@ -1,5 +1,5 @@
 package br.inatel.lista5;
-
+import
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,19 +11,22 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Path arquivo = Paths.get("jogos.txt");
+        Path arquivo = Paths.get("funcionarios.txt");
 
         try {
             List<String> conteudo = Files.readAllLines(arquivo);
             Map<String,String> empresa = new HashMap<>();
+            List<Produto> produto= new List<Produto>()
 
+            Empresários emp = new Empresários();
             conteudo.forEach((linha) -> {
                 String[] linhaQuebrada = linha.split(";");
                 empresa.put(linhaQuebrada[0],linhaQuebrada[1].strip());
             });
 
-            empresa.forEach((jogo,empresa) -> {
-                System.out.println("Jogo: " + jogo);
+            empresa.forEach((jogo,empresas) -> {
+                System.out.println("nome " + emp.isFilho());
+                if(emp.isFilho()!=)
                 System.out.println("Empresa: " + empresa);
                 System.out.println("");
             });
